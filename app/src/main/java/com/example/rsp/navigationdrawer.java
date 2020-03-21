4d4c6c45d771d3d3d146fb1c37f466303b000cce
usamaali04 <usamashiekh33@gmail.com>
2020-03-21 21:53:50 +0500

    setting design and bottom navigations

--- app/src/main/java/com/example/rsp/navigationdrawer.java
+++ app/src/main/java/com/example/rsp/navigationdrawer.java
@@ -50,9 +50,6 @@ public class navigationdrawer extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
