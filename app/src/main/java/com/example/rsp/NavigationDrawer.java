5ac683fe80ddf99eba4064b77f2e61b5fc497af1
usamaali04 <usamashiekh33@gmail.com>
2020-04-12 23:47:51 +0500

    Profile image setting

--- app/src/main/java/com/example/rsp/NavigationDrawer.java
+++ app/src/main/java/com/example/rsp/NavigationDrawer.java
@@ -1,5 +1,9 @@
 package com.example.rsp;
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -18,6 +22,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,17 +42,37 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
+
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -55,19 +80,20 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
     private static final int GALLARY_PICK = 1;
     private AppBarConfiguration mAppBarConfiguration;
     private ImageView imageView;
-   private Uri imageUri;
-   private StorageReference reference;
-   private DatabaseReference mDatabase;
-    String haha ;
+    final static int Gallery_pick = 1 ;
+    private StorageReference profileimagesref ;
+    private Uri imageUri;
+    CircleImageView profileimage ;
     DrawerLayout drawer;
     View headerview ;
-    CircleImageView circleImageView ;
     TextView name , email ;
     ActionBarDrawerToggle actionBarDrawerToggle ;
-    ImageView imageView , vechiles , dresses , furniture , property,accessories;
+    ImageView  vechiles , dresses , furniture , property,accessories;
     FirebaseAuth firebaseAuth ;
-    DatabaseReference firebaseDatabase ;
+    DatabaseReference userref ;
     String Currentuser ;
+    ProgressDialog progressDialog ;
+    String downloadUrl ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,19 +101,19 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
         setContentView(R.layout.activity_navigationdrawer);
         Toolbar toolbar = findViewById(R.id.toolbar);
         imageUri=null;
-        mDatabase=FirebaseDatabase.getInstance().getReference().child("Images");
-        reference=FirebaseStorage.getInstance().getReference().child("Images");
         setSupportActionBar(toolbar);
         imageView = findViewById(R.id.catelectronics);
         vechiles = findViewById(R.id.catvehicle);
         dresses = findViewById(R.id.catdresses);
+        progressDialog = new ProgressDialog(this);
         furniture = findViewById(R.id.catfurniture);
         property = findViewById(R.id.catproperty);
         accessories = findViewById(R.id.cataccessories);
         drawer = findViewById(R.id.drawer_layout);
         firebaseAuth = FirebaseAuth.getInstance();
         Currentuser = firebaseAuth.getCurrentUser().getUid() ;
-        firebaseDatabase = FirebaseDatabase.getInstance().getReference().child("User");
+        userref = FirebaseDatabase.getInstance().getReference().child("User");
+        profileimagesref = FirebaseStorage.getInstance().getReference().child("ProfileImages");
         actionBarDrawerToggle = new ActionBarDrawerToggle(this , drawer , R.string.open , R.string.close);
         drawer.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
@@ -149,6 +175,8 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
 //            }
 //        });
 
+        progressDialog = new ProgressDialog(this);
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         headerview = navigationView.getHeaderView(0);
         navigationView.setNavigationItemSelectedListener(this);
@@ -161,35 +189,18 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
                 .build();
 
         headerview = navigationView.getHeaderView(0);
-
         name = headerview.findViewById(R.id.txt_fname);
         email = headerview.findViewById(R.id.txt_email);
-        ImageView imageView=headerview.findViewById(R.id.profile_image);
-        Button button=headerview.findViewById(R.id.btn_AddImage);
-        button.setOnClickListener(new View.OnClickListener() {
+        profileimage = headerview.findViewById(R.id.profile_image);
+        profileimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent,"Select Image"),GALLARY_PICK);
+                requestStoragePermission();
             }
         });
 
 
-firebaseDatabase.child(Currentuser).addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-    }
-
-    @Override
-    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-    }
-});
-
-
-        firebaseDatabase.child(Currentuser).addValueEventListener(new ValueEventListener() {
+        userref.child(Currentuser).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists())
@@ -197,10 +208,14 @@ firebaseDatabase.child(Currentuser).addValueEventListener(new ValueEventListener
                     String Name = dataSnapshot.child("FullName").getValue().toString();
                     String Email = dataSnapshot.child("Email").getValue().toString();
 
+                    if (dataSnapshot.hasChild("Profile Image"))
+                    {
+                        String image = dataSnapshot.child("Profile Image").getValue().toString();
+                        Picasso.get().load(image).placeholder(R.drawable.ic_person_black_24dp).into(profileimage);
+                    }
                     name.setText(Name);
                     email.setText(Email);
                 }
-
             }
 
             @Override
@@ -210,33 +225,10 @@ firebaseDatabase.child(Currentuser).addValueEventListener(new ValueEventListener
         });
     }
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==GALLARY_PICK && resultCode==RESULT_OK){
-            imageUri=data.getData();
-            StorageReference reference1=reference.child("Image"+".jpg");
-            reference1.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    final String download=taskSnapshot.getDownloadUrl().toString();
-                    Map<String, String> map=new HashMap<>();
-                    map.put("image",downloadUrl);
-                    mDatabase.setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                Picasso.with(NavigationDrawer.this).load(downloadUrl).intro(imageView);
-                            }
-                        }
-                    });
-                }
-            });
-        }
-    }
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.navigationdrawer, menu);
+
         return true;
     }
 
@@ -246,4 +238,159 @@ firebaseDatabase.child(Currentuser).addValueEventListener(new ValueEventListener
 
         return true;
     }
+
+    private void requestStoragePermission() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            requestCameraPermission();
+                            Toast.makeText(getApplicationContext(), "All permissions are granted!", Toast.LENGTH_SHORT).show();
+                            Intent Gallery = new Intent();
+                            Gallery.setAction(Intent.ACTION_GET_CONTENT);
+                            Gallery.setType("image/*");
+                            startActivityForResult(Gallery , Gallery_pick);
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+    private void requestCameraPermission() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        // permission is granted
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        // check for permanent denial of permission
+                        if (response.isPermanentlyDenied()) {
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(NavigationDrawer.this);
+        builder.setTitle("Need Permissions");
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                openSettings();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == Gallery_pick && resultCode == RESULT_OK && data != null)
+        {
+
+            Uri resuluri = data.getData();
+
+            profileimage.setImageURI(resuluri);
+
+            progressDialog.setTitle("Setting Profile Picture");
+            progressDialog.setMessage("Please Wait.....");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            final StorageReference filepath = profileimagesref.child(Currentuser + ".jpg");
+
+            filepath.putFile(resuluri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    final Task<Uri> firebaseUri = taskSnapshot.getStorage().getDownloadUrl();
+                    firebaseUri.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            downloadUrl = uri.toString();
+                            userref.child(Currentuser).child("Profile Image").setValue(downloadUrl).addOnCompleteListener(
+
+                                    new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful())
+                                            {
+                                                progressDialog.dismiss();
+                                                Toast.makeText(NavigationDrawer.this, "Image uploaded Successfully", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                            );
+
+
+
+                        }
+                    });
+
+                }
+            });
+        }
+
+
+
+    }
+
+
 }
