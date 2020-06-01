53292de9ac8bcc550474736a351a71458766fcec
nabilalatif04 <nabilalatif978@gmail.com>
2020-06-01 23:41:58 +0500

    hello

---
+++ app/src/main/java/com/example/rsp/Post.java
@@ -0,0 +1,14 @@
+package com.example.rsp;
+
+public class Post {
+    String Title, Description,Ownername, Owneraddress,Mobilenumber;
+
+    public Post(String Title, String description, String ownername, String owneraddress, String Mobilenumber) {
+        this.Title = Title;
+        this.Description = Description;
+        this. Ownername = Ownername;
+        this.Owneraddress = Owneraddress;
+       this. Mobilenumber=Mobilenumber;
+    }
+}
+
