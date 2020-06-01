de1fd09ed6f204162ce565dfeef158e4890c94f1
usamaali04 <usamashiekh33@gmail.com>
2020-06-02 00:00:14 +0500

    post added

--- app/src/main/java/com/example/rsp/Post.java
+++ app/src/main/java/com/example/rsp/Post.java
@@ -1,14 +1,17 @@
 package com.example.rsp;
 
 public class Post {
-    String Title, Description,Ownername, Owneraddress,Mobilenumber;
+    public String Title, Description,Ownername, Owneraddress,Mobilenumber;
 
-    public Post(String Title, String description, String ownername, String owneraddress, String Mobilenumber) {
-        this.Title = Title;
-        this.Description = Description;
-        this. Ownername = Ownername;
-        this.Owneraddress = Owneraddress;
-       this. Mobilenumber=Mobilenumber;
+    public Post() {
+    }
+
+    public Post(String title, String description, String ownername, String owneraddress, String mobilenumber) {
+        Title = title;
+        Description = description;
+        Ownername = ownername;
+        Owneraddress = owneraddress;
+        Mobilenumber = mobilenumber;
     }
 }
 
