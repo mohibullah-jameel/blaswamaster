dd27121c95c5a3d616910ae4ecded55ce5ed5c16
Usama Ali <usamashiekh33@gmail.com>
2020-07-26 02:20:51 +0500

    send

--- app/src/main/java/com/example/rsp/ManageAccount.java
+++ app/src/main/java/com/example/rsp/ManageAccount.java
@@ -52,86 +52,12 @@ import java.util.Calendar;
 import java.util.HashMap;
 import de.hdodenhof.circleimageview.CircleImageView;
 public class ManageAccount extends AppCompatActivity {
-    private EditText et_Current_pw,et_New_pw,et_Full_nm,et_email_address;
-    private Button btn_Confirm;
-    DatabaseReference mRef;
-    private SharedPreferences sharedPreferences;
-    private  String cPass,nPass;
-    private  String getPassword,getFullName,getEmailAddress,getNumber;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manageaccount);
-        initViews();
-        initListener();
-
     }
-    private void initViews() {
-
-        et_Current_pw=findViewById(R.id.current_password);
-        et_New_pw=findViewById(R.id.new_password);
-        et_Full_nm=findViewById(R.id.full_name);
-        et_email_address=findViewById(R.id.email_address);
-        btn_Confirm=findViewById(R.id.btnconfirm);
-        sharedPreferences=getSharedPreferences(Constraints.AUTOFILL_HINT_USERNAME,MODE_PRIVATE);
-
-    }
-
-    private void initListener() {
-        btn_Confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isValid()){
-                    Update();
-                }
-            }
-        });
-    }
-    private void Update(){
-        cPass=et_Current_pw.getText().toString();
-        nPass=et_New_pw.getText().toString();
-        getPassword=sharedPreferences.getString(Constraints.AUTOFILL_HINT_PASSWORD,"");
-        getFullName=sharedPreferences.getString(Constraints.AUTOFILL_HINT_NAME,"");
-        getEmailAddress = sharedPreferences.getString(Constraints.AUTOFILL_HINT_EMAIL_ADDRESS,"");
-
-        mRef=FirebaseDatabase.getInstance().getReference("User");
-        mRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot childDataSnapShot:dataSnapshot.getChildren()){
-                    User user=childDataSnapShot.getValue(User.class);
-
-                    if (user.Number.equals(getNumber)&&user.Password.equals(cPass)){
-                        mRef.child(user.Number).child("password").setValue(nPass);
-                        Toast.makeText(ManageAccount.this,"Successfully Updated",Toast.LENGTH_LONG).show();
-                    }
-                    else if (!user.Password.equals(cPass)){
-                        Snackbar.make(getWindow().getDecorView().getRootView(),"Current Paaword",Snackbar.LENGTH_LONG);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-    }
-private boolean isValid() {
-    cPass = et_Current_pw.getText().toString();
-    nPass = et_New_pw.getText().toString();
-    if (cPass.equals("")) {
-        et_Current_pw.setError(Constraints.AUTOFILL_HINT_PASSWORD);
-        return false;
-    }
-    if (nPass.equals("")) {
-        et_New_pw.setError(Constraints.AUTOFILL_HINT_PASSWORD);
-        return false;
-    }
-    return true;
-}
 
 }
--- app/src/main/java/com/example/rsp/User.java
+++ app/src/main/java/com/example/rsp/User.java
@@ -2,22 +2,16 @@ package com.example.rsp;
 
 public class User {
     public  String  FullName,EmailAddress;
-    public String Number;
-    public String Password;
 
 
 
     public User(){
 
     }
-    public User(String fullname,String emailAddress,String password,String number){
 
-        FullName=fullname;
-        EmailAddress=emailAddress;
-        Password=password;
-        Number=number;
-
-
+    public User(String fullName, String emailAddress) {
+        FullName = fullName;
+        EmailAddress = emailAddress;
     }
 }
 
